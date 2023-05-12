25ef093184e87bb85d66690e1c8142c79a5f5b7a
KevinAxelN <123051306+KevinAxelN@users.noreply.github.com>
2023-05-12 16:24:25 +0700

    Add files via upload

---
+++ Web_Portofolio/script.js
@@ -0,0 +1,14 @@
+let home = document.getElementById("home");
+let about = document.getElementById("about");
+let contact = document.getElementById("contact");
+let portfolio = document.getElementById("portfolio");
+
+// console.log(about);
+
+about.addEventListener("click", (e) => {
+  about.classList.add("aktif");
+  console.log(about);
+});
+
+alert("hello!");
+console.log("HEllo");
